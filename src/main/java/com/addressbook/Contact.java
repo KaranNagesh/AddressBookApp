e6911a6b07870298847259a1fd733d63513a517a
KaranNagesh <Karannagesh86@gmail.com>
2026-03-09 00:15:00 +0530

    [Karan] : implemented UC2 feature of adding contacts in address book app

--- src/main/java/com/addressbook/Contact.java
+++ src/main/java/com/addressbook/Contact.java
@@ -39,7 +39,7 @@ public class Contact {
     public String getCity() { return city; }
     public void setCity(String city) { this.city = city; }
 
-    public String getState() { return state; }	
+    public String getState() { return state; }
     public void setState(String state) { this.state = state; }
 
     public String getZip() { return zip; }
